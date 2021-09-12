f7589eb2e37573c65100e014ba480f29f35f8561
Alfaa123 <0xxxalexanderxxx0@gmail.com>
2021-09-12 16:32:43 -0400

    Create Arduino_Thermostat.ino

---
+++ Firmware/Arduino_Thermostat/Arduino_Thermostat.ino
@@ -0,0 +1,20 @@
+#define backlight 10
+
+#include <LiquidCrystal.h>
+#include <BigNums2x2.h>
+
+
+LiquidCrystal lcd(A1, A0, A5, A4, A3, A2);
+BigNums2x2 bigNum(TREK);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  pinMode(backlight, OUTPUT);
+  digitalWrite(backlight, HIGH);
+  lcd.begin(16, 2);
+  bigNum.print(70);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+}
